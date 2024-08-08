fc9fa9f3e63403f6197b6906387a80aae0c4dcca
vishaalcodre <vishaa369@gmail.com>
2024-08-08 15:21:38 +0530

    UI update with react-chat-pretty

--- nodejs-reactjs-chat/frontend/src/ChatsPage.jsx
+++ nodejs-reactjs-chat/frontend/src/ChatsPage.jsx
@@ -2,13 +2,14 @@ import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-
 import { PrettyChatWindow } from "react-chat-engine-pretty"
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        'fb303024-d7ed-41b5-a1fb-ce29674c6e74', props.user.username, props.user.secret
-    );
     return(
     <div style={{height: '100vh'}}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+        <PrettyChatWindow 
+        projectId="fb303024-d7ed-41b5-a1fb-ce29674c6e74"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{height: '100%'}}
+        />
     </div>
 );
 }
